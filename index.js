3fd2ccbe584c5265ce26dcb54fe44bd45d2b89e9
Deepak kumar <dk0778671@gmail.com>
2024-10-17 18:49:22 +0530

    update.README

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express= require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const {v4: uuidv4} =require('uuid');
+const methodOverride =require("method-override")
 
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
@@ -14,17 +17,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts =[
     {
-        id: "1a",
+        id: uuidv4(),
         username : "Deepakkumar",
         content : " i love coding",
     },
     {
-        id: "2b",
+        id: uuidv4(),
         username : "Varsha",
         content : " i love medical",
     },
     {
-        id:"3c",
+        id:uuidv4(),
         username : "Anjali",
         content : " i love my family",
     },
@@ -41,8 +44,10 @@ app.post("/posts",(req,res) =>{
     // console.log(req.body);
 
     // to add data into array
+    
     let {username,content} =req.body;
-    posts.push({username,content});
+    let id = uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
 });
 
@@ -54,6 +59,36 @@ app.get("/posts/:id",(req,res) =>{
     res.render("show.ejs",{post});
 
 });
+
+// PATCH 
+
+app.patch("/posts/:id",(req,res) => {
+    let {id} =req.params;
+    let newContent = req.body.content;
+
+    // post ke data ko update
+    let post =posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+
+});
+
+app.get("/posts/:id/edit",(req,res) => {
+    let {id} =req.params;
+    let post =posts.find((p) => id === p.id);
+    res.render("edit.ejs",{post});
+
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");  // Redirect back to the main post listing
+});
+
+
+
 
 
 app.listen(port, () =>{
